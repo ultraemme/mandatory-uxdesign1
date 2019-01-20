3f01d89ade93be74a0a1462cd7e11ffa62318ec8
ultraemme <e.larsson1994@gmail.com>
2019-01-20 01:10:21 +0100

    working on textfield

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mandatory-uxdesign1
+
+TODO:
+
+ease in - fade out
+
+fix input text sizing, margins, paddings etc
+
+but first, let me take a nap
--- mds.js
+++ mds.js
@@ -1,23 +1,37 @@
 window.mds = {
   textField: function (element) {
-    element.addEventListener('click', function (e) {
-      element.style.color = "red";
-      console.log(element);
+    let label = document.querySelector(".mds-text-field__label");
+    let indicator = document.querySelector(".mds-text-field__indicator");
+    element.addEventListener("focus", function (e) {
+      indicator.style.transform = "scaleX(1)";
+      label.style.color = "#6200ee";
+      label.style.lineHeight = "32px";
+      label.style.fontSize = "12px";
+      label.style.opacity = "1";
+    })
+    element.addEventListener("blur", function (e) {
+      if (!e.target.value) {
+        indicator.style.transform = "scaleX(0)";
+        label.style.color = null;
+        label.style.lineHeight = "56px";
+        label.style.fontSize = null;
+        label.style.opacity = "0.6";
+      }
     })
   },
   switch: function (element) {
-    element.addEventListener('click', function (e) {
+    element.addEventListener("click", function (e) {
       element.style.color = "blue";
       console.log(element);
     })
   },
   checkbox: function (element) {
-    element.addEventListener('click', function (e) {
+    element.addEventListener("click", function (e) {
       console.log(element);
     })
   },
   radioButton: function (element) {
-    element.addEventListener('click', function (e) {
+    element.addEventListener("click", function (e) {
       console.log(element);
     })
   }
